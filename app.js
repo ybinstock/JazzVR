ab0d2edb901ad270c1c2757ba9cd05fdf7e578f2
ybinstock <ybinstock@gmail.com>
2017-02-25 14:14:56 -0500

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+//For eyes
+  function toggleSigns() {
+    document.getElementById('instructions').setAttribute('visible', 'false');
+  }
+
+  setTimeout(toggleSigns, 4000);
+
+});
